21bab1d6eb3f80f0d768dd9ff322d8b05357d14f
diana-vahomskaya <diana.vahomskaya@onlyoffice.com>
2024-10-03 14:53:43 +0300

    master

---
+++ Createfile/Createfile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
+
+string currentDirectory = Directory.GetCurrentDirectory();
+
+string projectPath = Path.GetFullPath(Path.Combine(currentDirectory, @"..\..\..\..\.."));
+string outputFilePath = Path.Combine(AppContext.BaseDirectory, "output.txt");
+var filePath = $"{projectPath}\\Name.txt";
+
+try
+{
+    string word = File.ReadAllText(filePath);
+    string result = $"{word} hello world";
+    File.WriteAllText(outputFilePath, result);
+    Console.WriteLine($"Содержимое файла: {word}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
